c34c01e20b9552e31677e0f947337e62c4f1df20
Camila-FR <camilafloresramirez@gmail.com>
2022-11-10 11:14:51 -0300

    avance menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,74 @@
-const Menu = () =>{
+import Card from 'react-bootstrap/Card';
+
+const Menu= () =>{
     return (
-        <div>
-            <h1>Vista Menu</h1>
-        </div>
+    <div>
+        <h1>Menu</h1>
+        <div className='container d-flex'>
+            <div className='row justify-content-center'>
+            <Card style={{ width: '18rem' }} className= "m-3">
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Pescado a lo Pobre</Card.Title>
+                    <Card.Text>
+                    Pescado con papas fritas, huevo y cebolla caramelizada
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }} className= "m-3">
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Pescado a lo Pobre</Card.Title>
+                    <Card.Text>
+                    Pescado con papas fritas, huevo y cebolla caramelizada
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }} className= "m-3">
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Pescado a lo Pobre</Card.Title>
+                    <Card.Text>
+                    Pescado con papas fritas, huevo y cebolla caramelizada
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }} className= "m-3">
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Pescado a lo Pobre</Card.Title>
+                    <Card.Text>
+                    Pescado con papas fritas, huevo y cebolla caramelizada
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }} className= "m-3">
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Pescado a lo Pobre</Card.Title>
+                    <Card.Text>
+                    Pescado con papas fritas, huevo y cebolla caramelizada
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }} className= "m-3">
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Pescado a lo Pobre</Card.Title>
+                    <Card.Text>
+                    Pescado con papas fritas, huevo y cebolla caramelizada
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            </div>
+         </div>
+           
+    </div>
     )
 }
 
